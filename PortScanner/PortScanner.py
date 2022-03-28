9624007d2f67f384ca482dea52366b7fb7733078
Sophia Larson <sillarson@Sophias-MacBook-Pro.local>
2022-03-28 13:23:56 -0400

    PortScanner.py; redid the main to run the ports without having to press enter

--- PortScanner/PortScanner.py
+++ PortScanner/PortScanner.py
@@ -12,24 +12,14 @@ def tcp_scanner(port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
-    #resolve IP from host name
-    try:
-        remote_server = input("Enter a remote host to scan: ")
-        remote_server_IP = socket.gethostbyname(remote_server)
-    except socket.gaierror:
-        #if host name not resolved, print error message then exit
-        print("Host name couldn't be resolved...")
-        sys.exit()
-    except KeyboardInterrupt:
-        print("Exiting...")
-        sys.exit()
-
-
     #basic port scanner
     try:
         res = sock.connect_ex((remote_server_IP, port))
         if res == 0:
             print("Port {}: OPEN".format(port))
+        else:
+            print("Port {}: CLOSED".format(port))
+        #closes after one closed port, need to fix
         sock.close()
 
     except socket.error:
@@ -42,9 +32,23 @@ def main():
     """
     The main class
     """
+    
    
 
 
 if __name__ == '__main__':
-    for port in range(100):
+        #resolve IP from host name
+    try:
+        remote_server = input("Enter a remote host to scan: ")
+        remote_server_IP = socket.gethostbyname(remote_server)
+    except socket.gaierror:
+        #if host name not resolved, print error message then exit
+        print("Host name couldn't be resolved...")
+        sys.exit()
+    except KeyboardInterrupt:
+        print("Exiting...")
+        sys.exit()
+
+    #scan a number of ports
+    for port in range(1000):
         tcp_scanner(port)
