9115cadacbfc25cc08643193035b62524d52d46a
Sophia Larson <sillarson@Sophias-MacBook-Pro.local>
2022-03-28 12:44:24 -0400

    PortScanner.py; added comments, created sock object, made sock.error an exception

--- PortScanner/PortScanner.py
+++ PortScanner/PortScanner.py
@@ -2,9 +2,24 @@ import socket
 import sys
 
 def tcp_scanner(port):
-    pass
+    """
+    A scanner that takes a parameter(port). 
+    This parameter is the port being scanned.
+    """
+    #sock is the name of the socket object to connect to ports
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+
+    except socket.error:
+        #when encountering an error, print error message then exit
+        print("Couldn\'t connect to server... ")
+        sys.exit()
 
 def main():
+    """
+    The main class
+    """
     pass
 
 if __name__ == '__main__':
